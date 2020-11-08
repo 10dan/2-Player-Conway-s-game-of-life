6647ded93eace04c444e843b00c139c687b61079
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-11-08 16:39:32 +0000

    Added how to play page and navigation menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    [SerializeField] GameObject mainMenu, howToPage;
+    public void ShowHowTo() {
+        mainMenu.active = false;
+        howToPage.active = true;
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMainMenu() {
+        mainMenu.active = true;
+        howToPage.active = false;
+    }
+}
