1c2fa6fe5c03c05312475dfa901a83a823b655d8
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-11-29 16:16:39 +0000

    Rules work

--- Assets/Scripts/BoardBehavior.cs
+++ Assets/Scripts/BoardBehavior.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardBehavior : MonoBehaviour {
     [SerializeField] GameObject cell; //Cell prefab.
-    [SerializeField] int numCellsVert = 20; //Number of cells vertically. (*2 for horizontal)
-    int numCellsHorz;
+    [SerializeField] int numCellsVert = 20; //Number of cells vertically.
+    int numCellsHorz; //= NumCellsVert * 2;
+
+    public static bool wrapAround = true;
+    public enum GameStates { Planning, Playing, Over };
+    public GameStates gameState = GameStates.Planning;
 
     //Structure that will store cells.
     Cell[,] cells;
@@ -14,7 +19,53 @@ public class BoardBehavior : MonoBehaviour {
         InitVariables();
         PlaceCells();
     }
+
+    private void InitVariables() {
+        //Init variables.
+        numCellsHorz = numCellsVert * 2;
+        cells = new Cell[numCellsHorz, numCellsVert];
+    }
+
     private void Update() {
+        CheckForInputs();
+        if (gameState == GameStates.Planning) {
+            CheckForCellSelection();
+        }
+        if (gameState == GameStates.Playing) {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                UpdateBoard();
+            }
+        }
+
+    }
+
+    private void CheckForInputs() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            gameState = GameStates.Playing;
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            gameState = GameStates.Planning;
+        }
+        //Press C to clear.
+        if (Input.GetKeyDown(KeyCode.C)) {
+            gameState = GameStates.Planning;
+            foreach(Cell c in cells) {
+                c.state = Cell.CellState.Dead;
+                c.nextState = Cell.CellState.Dead;
+            }
+        }
+    }
+
+    private void UpdateBoard() {
+        foreach(Cell c in cells) {
+            c.CalcNextState(cells);
+        }
+        foreach (Cell c in cells) {
+            c.state = c.nextState;
+        }
+    }
+
+    private void CheckForCellSelection() {
         GameObject selected = RayCastScreen();
         if (selected != null) {
             if (selected.GetComponent<Cell>() != null) {
@@ -40,22 +91,16 @@ public class BoardBehavior : MonoBehaviour {
     }
 
 
-    private void InitVariables() {
-        //Init variables.
-        numCellsHorz = numCellsVert * 2;
-        cells = new Cell[numCellsHorz, numCellsVert];
-    }
-
     private void PlaceCells() {
         //Calc the neccessary size for the cells.
         Vector3 boardSize = gameObject.transform.localScale;
-        float newCellScale = (boardSize.y / numCellsVert) / 1.1f;
+        float newCellScale = (boardSize.y / numCellsVert) / 1.2f;
         Vector3 newCellScaleVector = new Vector3(newCellScale, newCellScale, 1f);
         cell.transform.localScale = newCellScaleVector;
 
         //Place the cells onto the board
         float d = boardSize.y / numCellsVert; //Distance between cells
-        for (int x = 0; x < numCellsVert * 2; x++) {
+        for (int x = 0; x < numCellsHorz; x++) {
             for (int y = 0; y < numCellsVert; y++) {
                 GameObject createdCell = Instantiate(cell, new Vector3(d / 2 + x * d, d / 2 + y * d, 0f), Quaternion.identity);
                 cells[x, y] = createdCell.GetComponent<Cell>();
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,14 +5,72 @@ using UnityEngine;
 public class Cell : MonoBehaviour {
 
     public enum CellState { Alive1, Alive2, Dead } //Potential states of cells. Alive1 means owned by player 1.
-    public CellState state = CellState.Dead; 
+    public CellState state = CellState.Dead;
+    public CellState nextState = CellState.Dead;
 
     public bool selected = false; //Is the player hovering this cell?
     public Vector2Int pos; //Holds the x,y index of the cell.
     [SerializeField] Material[] cols; //Holds the possible colours of cells.
 
+
+    TextMesh text;
+    private void Start() {
+        text = GetComponentInChildren<TextMesh>();
+    }
+
     private void Update() {
         UpdateColour();
+    }
+    public void CalcNextState(Cell[,] cells) {
+        int x = pos.x;
+        int y = pos.y;
+        //Count the number of cells alive per team.
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = -1; i <= 1; i++) {
+            int k = (x + i + cells.GetLength(0)) % cells.GetLength(0);
+            for (int j = -1; j <= 1; j++) {
+                int h = (y + j + cells.GetLength(1)) % cells.GetLength(1);
+                //If wrap around is disabled, check if we are out of bounds.
+                if (BoardBehavior.wrapAround == false) {
+                    if ((x + i < 0) || (x + i >= cells.GetLength(0) ||
+                        (y + j < 0) || (y + j >= cells.GetLength(1)))) {//Out of bounds.
+                    } else {
+                        if (cells[x + i, y + j].state == Cell.CellState.Alive1) {
+                            count1++;
+                        } else if (cells[x + i, y + j].state == Cell.CellState.Alive2) {
+                            count2++;
+                        }
+                    }
+                } else {
+                    if (cells[k, h].state == Cell.CellState.Alive1) {
+                        count1++;
+                    } else if (cells[k, h].state == Cell.CellState.Alive2) {
+                        count2++;
+                    }
+                }
+            }
+        }
+        int totalCount = count1 + count2;
+        //To avoid counting itself.
+        if (cells[x, y].state != Cell.CellState.Dead) totalCount--;
+        //Display text for debugging.
+        text.text = totalCount.ToString();
+        if (state != CellState.Dead) {
+            if (totalCount > 3 || totalCount < 2) {
+                nextState = CellState.Dead;
+            } else if (totalCount == 2 || totalCount == 3) {
+                nextState = state;
+            }
+        } else {
+            if (totalCount == 3) {
+                if (count1 > count2) {
+                    nextState = CellState.Alive1;
+                } else {
+                    nextState = CellState.Alive2;
+                }
+            }
+        }
     }
 
     private void UpdateColour() {
